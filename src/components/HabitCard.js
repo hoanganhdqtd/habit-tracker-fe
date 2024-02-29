0193de8c39230392097f7b18f7afbe8027de3a63
Anh Phan <anhphan@Anhs-MacBook-Pro.local>
2024-02-29 22:51:52 +0700

    Change habit title width for small screen

--- src/components/HabitCard.js
+++ src/components/HabitCard.js
@@ -241,7 +241,7 @@ function HabitCard({ habit, isInCalendarPage, date }) {
                 navigate(`/habit/${habit._id}`);
               }}
               sx={{
-                width: smScreenUp ? "inherit" : "50px",
+                width: smScreenUp ? "inherit" : "100px",
               }}
             >
               {habit.name}
